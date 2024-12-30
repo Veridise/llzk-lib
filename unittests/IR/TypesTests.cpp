b3fe03e07aede3f42080a6f0c50ba09a9b7d4f20
Tim Hoffman <timothy.hoffman@veridise.com>
2024-12-30 12:37:23 -0600

    attempt to fix gtest error

--- unittests/IR/TypesTests.cpp
+++ unittests/IR/TypesTests.cpp
@@ -3,55 +3,54 @@
 #include <gtest/gtest.h>
 
 using namespace llzk;
+using namespace mlir;
 
 class TypeTests : public ::testing::Test {
 public:
-  mlir::MLIRContext ctx;
-  mlir::Builder bldr;
+  MLIRContext ctx;
 
 protected:
-  TypeTests() : ctx(), bldr(&ctx) { ctx.loadDialect<llzk::LLZKDialect>(); }
-
-  void SetUp() override { bldr = mlir::Builder(&ctx); }
+  TypeTests() : ctx() { ctx.loadDialect<llzk::LLZKDialect>(); }
 };
 
 #define TEST_F_WITH_DIAGNOSTIC(test_fixture, test_name, body_func, expected_diagnostic)            \
   TEST_F(test_fixture, test_name) {                                                                \
     std::string capturedDiagnostic;                                                                \
-    ctx.getDiagEngine().registerHandler([&](mlir::Diagnostic &diag) {                              \
+    auto h = ctx.getDiagEngine().registerHandler([&](Diagnostic &diag) {                           \
       llvm::raw_string_ostream stream(capturedDiagnostic);                                         \
       diag.print(stream);                                                                          \
-      return mlir::success();                                                                      \
+      return success();                                                                            \
     });                                                                                            \
     (body_func);                                                                                   \
     EXPECT_EQ(capturedDiagnostic, expected_diagnostic);                                            \
+    ctx.getDiagEngine().eraseHandler(h);                                                           \
   }
 
 TEST_F(TypeTests, testCloneSuccessNewType) {
-  auto tyBool = bldr.getI1Type();
-  auto tyIndex = bldr.getIndexType();
-  auto a = ArrayType::get(tyIndex, {2, 2});
-  auto b = a.cloneWith(std::nullopt, tyBool);
+  IntegerType tyBool = IntegerType::get(&ctx, 1);
+  IndexType tyIndex = IndexType::get(&ctx);
+  ArrayType a = ArrayType::get(tyIndex, {2, 2});
+  ArrayType b = a.cloneWith(std::nullopt, tyBool);
   ASSERT_EQ(b.getElementType(), tyBool);
-  ASSERT_EQ(b.getShape(), mlir::ArrayRef(std::vector<int64_t>({2, 2})));
+  ASSERT_EQ(b.getShape(), ArrayRef(std::vector<int64_t>({2, 2})));
 }
 
 TEST_F(TypeTests, testCloneSuccessNewShape) {
-  auto tyIndex = bldr.getIndexType();
-  auto a = ArrayType::get(tyIndex, {2, 2});
+  IndexType tyIndex = IndexType::get(&ctx);
+  ArrayType a = ArrayType::get(tyIndex, {2, 2});
   std::vector<int64_t> newShapeVec({2, 3, 2});
-  mlir::ArrayRef newShape(newShapeVec);
-  auto b = a.cloneWith(std::make_optional(newShape), tyIndex);
+  ArrayRef newShape(newShapeVec);
+  ArrayType b = a.cloneWith(std::make_optional(newShape), tyIndex);
   ASSERT_EQ(b.getElementType(), tyIndex);
   ASSERT_EQ(b.getShape(), newShape);
 }
 
 void testVerifyErrorEmptyShapeImpl(TypeTests *test) {
-  auto tyIndex = test->bldr.getIndexType();
-  auto a = ArrayType::get(tyIndex, {2, 2});
+  IndexType tyIndex = IndexType::get(&test->ctx);
+  ArrayType a = ArrayType::get(tyIndex, {2, 2});
   std::vector<int64_t> newShapeVec;
-  mlir::ArrayRef newShape(newShapeVec);
-  auto b = a.cloneWith(std::make_optional(newShape), tyIndex);
+  ArrayRef newShape(newShapeVec);
+  ArrayType b = a.cloneWith(std::make_optional(newShape), tyIndex);
   ASSERT_EQ(b, nullptr);
 }
 
