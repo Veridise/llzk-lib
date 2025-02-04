a208e9e986778d7bbd67d158bf0554724a5b11c4
Tim Hoffman <timothy.hoffman@veridise.com>
2025-02-04 10:26:06 -0600

    fix mem leak

--- unittests/IR/AffineMapInstantiationTests.cpp
+++ unittests/IR/AffineMapInstantiationTests.cpp
@@ -1,3 +1,4 @@
+#include "llzk/Dialect/LLZK/IR/Builders.h"
 #include "llzk/Dialect/LLZK/IR/Ops.h"
 
 #include <mlir/Dialect/Index/IR/IndexOps.h>
@@ -11,22 +12,33 @@ class AffineMapInstantiationTests : public ::testing::Test {
 public:
   MLIRContext ctx;
   Location loc;
+  OwningOpRef<ModuleOp> mod;
 
 protected:
-  AffineMapInstantiationTests() : ctx(), loc(UnknownLoc::get(&ctx)) {
+  AffineMapInstantiationTests() : ctx(), loc(UnknownLoc::get(&ctx)), mod() {
     ctx.loadDialect<llzk::LLZKDialect>();
+  }
+
+  void SetUp() override {
+    // Create a new module for each test
+    mod = createLLZKModule(&ctx, loc);
+  }
+
+  void TearDown() override {
+    // Allow existing module to be erased after each test
+    mod = OwningOpRef<ModuleOp>();
   }
 };
 
-TEST_F(AffineMapInstantiationTests, testElementInitEmpty) {
-  OpBuilder bldr(&ctx);
+TEST_F(AffineMapInstantiationTests, testElementInit_GoodEmpty) {
+  OpBuilder bldr(mod->getRegion());
   ArrayType arrTy = ArrayType::get(bldr.getIndexType(), {2, 2}); // !llzk.array<2,2 x index>
   CreateArrayOp op = bldr.create<CreateArrayOp>(loc, arrTy);
   ASSERT_TRUE(succeeded(op.verifyInvariants()));
 }
 
-TEST_F(AffineMapInstantiationTests, testElementInitNonEmpty) {
-  OpBuilder bldr(&ctx);
+TEST_F(AffineMapInstantiationTests, testElementInit_GoodNonEmpty) {
+  OpBuilder bldr(mod->getRegion());
   ArrayType arrTy = ArrayType::get(bldr.getIndexType(), {2}); // !llzk.array<2 x index>
   auto v1 = bldr.create<index::ConstantOp>(loc, 766);
   auto v2 = bldr.create<index::ConstantOp>(loc, 562);
@@ -34,13 +46,13 @@ TEST_F(AffineMapInstantiationTests, testElementInitNonEmpty) {
   ASSERT_TRUE(succeeded(op.verifyInvariants()));
 }
 
-TEST_F(AffineMapInstantiationTests, testElementInitTooFew) {
+TEST_F(AffineMapInstantiationTests, testElementInit_TooFew) {
+  OpBuilder bldr(mod->getRegion());
+  ArrayType arrTy = ArrayType::get(bldr.getIndexType(), {5}); // !llzk.array<5 x index>
+  auto v1 = bldr.create<index::ConstantOp>(loc, 766);
+  auto v2 = bldr.create<index::ConstantOp>(loc, 562);
   EXPECT_DEATH(
       {
-        OpBuilder bldr(&ctx);
-        ArrayType arrTy = ArrayType::get(bldr.getIndexType(), {5}); // !llzk.array<5 x index>
-        auto v1 = bldr.create<index::ConstantOp>(loc, 766);
-        auto v2 = bldr.create<index::ConstantOp>(loc, 562);
         CreateArrayOp op = bldr.create<CreateArrayOp>(loc, arrTy, ValueRange({v1, v2}));
         assert(succeeded(op.verifyInvariants()));
       },
@@ -48,13 +60,13 @@ TEST_F(AffineMapInstantiationTests, testElementInitTooFew) {
   );
 }
 
-TEST_F(AffineMapInstantiationTests, testElementInitTooMany) {
+TEST_F(AffineMapInstantiationTests, testElementInit_TooMany) {
+  OpBuilder bldr(mod->getRegion());
+  ArrayType arrTy = ArrayType::get(bldr.getIndexType(), {1}); // !llzk.array<1 x index>
+  auto v1 = bldr.create<index::ConstantOp>(loc, 766);
+  auto v2 = bldr.create<index::ConstantOp>(loc, 562);
   EXPECT_DEATH(
       {
-        OpBuilder bldr(&ctx);
-        ArrayType arrTy = ArrayType::get(bldr.getIndexType(), {1}); // !llzk.array<1 x index>
-        auto v1 = bldr.create<index::ConstantOp>(loc, 766);
-        auto v2 = bldr.create<index::ConstantOp>(loc, 562);
         CreateArrayOp op = bldr.create<CreateArrayOp>(loc, arrTy, ValueRange({v1, v2}));
         assert(succeeded(op.verifyInvariants()));
       },
@@ -62,12 +74,12 @@ TEST_F(AffineMapInstantiationTests, testElementInitTooMany) {
   );
 }
 
-TEST_F(AffineMapInstantiationTests, testElementInitWithAffineMapType) {
+TEST_F(AffineMapInstantiationTests, testElementInit_WithAffineMapType) {
+  OpBuilder bldr(mod->getRegion());
+  AffineMapAttr m = AffineMapAttr::get(bldr.getDimIdentityMap()); // (d0) -> (d0)
+  ArrayType arrTy = ArrayType::get(bldr.getIndexType(), {m});     // !llzk.array<#m x index>
   EXPECT_DEATH(
       {
-        OpBuilder bldr(&ctx);
-        AffineMapAttr m = AffineMapAttr::get(bldr.getDimIdentityMap()); // (d0) -> (d0)
-        ArrayType arrTy = ArrayType::get(bldr.getIndexType(), {m});     // !llzk.array<#m x index>
         CreateArrayOp op = bldr.create<CreateArrayOp>(loc, arrTy);
         assert(succeeded(op.verifyInvariants()));
       },
