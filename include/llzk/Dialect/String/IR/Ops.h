9ede26448bf37c8caa5f39631ec04a9e6efa167c
Ian Glen Neal <ian.gl.neal@gmail.com>
2025-04-29 12:38:35 -0400

    Partial `string` refactor

---
+++ include/llzk/Dialect/String/IR/Ops.h
@@ -0,0 +1,20 @@
+//===-- Ops.h ---------------------------------------------------*- C++ -*-===//
+//
+// Part of the LLZK Project, under the Apache License v2.0.
+// See LICENSE.txt for license information.
+// Copyright 2025 Veridise Inc.
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "llzk/Dialect/String/IR/Dialect.h"
+#include "llzk/Dialect/String/IR/Types.h"
+
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/Location.h>
+
+// Include TableGen'd declarations
+#define GET_OP_CLASSES
+#include "llzk/Dialect/String/IR/Ops.h.inc"
