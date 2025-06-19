f7d616fddadbb452969e2e01fae4d7d73779e125
Daniel Dominguez <daniel@veridise.com>
2025-06-19 17:58:42 +0200

    Part of the unit tests

---
+++ unittests/CAPI/Dialect/Global.cpp
@@ -0,0 +1,8 @@
+//===-- Global.cpp ----------------------------------------------*- C++ -*-===//
+//
+// Part of the LLZK Project, under the Apache License v2.0.
+// See LICENSE.txt for license information.
+// Copyright 2025 Veridise Inc.
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
