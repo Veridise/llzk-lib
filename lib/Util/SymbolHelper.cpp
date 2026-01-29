83ac08e8ae803d18077bae7f2980425cf0fa0914
Tim Hoffman <timothy.hoffman@veridise.com>
2026-01-29 16:18:31 -0600

    ensure it's a concrete StructType attr

--- lib/Util/SymbolHelper.cpp
+++ lib/Util/SymbolHelper.cpp
@@ -312,12 +312,17 @@ FailureOr<StructType> getMainInstanceType(Operation *lookupFrom) {
   }
   ModuleOp root = rootOpt.value();
   if (Attribute a = root->getAttr(MAIN_ATTR_NAME)) {
-    // If the attribute is present, it must be a TypeAttr of StructType.
+    // If the attribute is present, it must be a TypeAttr of concrete StructType.
     if (TypeAttr ta = llvm::dyn_cast<TypeAttr>(a)) {
-      return success(llvm::dyn_cast<StructType>(ta.getValue()));
+      if (StructType st = llvm::dyn_cast<StructType>(ta.getValue())) {
+        if (isConcreteType(st)) {
+          return success(st);
+        }
+      }
     }
     return rootOpt->emitError().append(
-        '"', MAIN_ATTR_NAME, "\" on top-level module must be a '", StructType::name, "' attribute"
+        '"', MAIN_ATTR_NAME, "\" on top-level module must be a concrete '", StructType::name,
+        "' attribute. Found: ", a
     );
   }
   // The attribute is optional so it's okay if not present.
