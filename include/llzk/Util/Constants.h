d3eef003b3a8bb1d7406f49bb294376cd722cd8a
Tim Hoffman <timothy.hoffman@veridise.com>
2026-01-29 13:35:43 -0600

    Specify main via attr instead of reserved name

--- include/llzk/Util/Constants.h
+++ include/llzk/Util/Constants.h
@@ -15,13 +15,6 @@ namespace llzk {
 /// to a circom signal or AIR/PLONK column, opposed to intermediate values or other expressions.
 constexpr char COMPONENT_NAME_SIGNAL[] = "Signal";
 
-/// Symbol name for the main entry point struct/component (if any). There are additional
-/// restrictions on the struct with this name:
-/// 1. It cannot have struct parameters.
-/// 2. The parameter types of its functions (besides the required "self" parameter) can
-///     only be `struct<Signal>` or `array<.. x struct<Signal>>`.
-constexpr char COMPONENT_NAME_MAIN[] = "Main";
-
 /// Symbol name for the witness generation (and resp. constraint generation) functions within a
 /// component.
 constexpr char FUNC_NAME_COMPUTE[] = "compute";
@@ -30,5 +23,10 @@ constexpr char FUNC_NAME_PRODUCT[] = "product";
 
 /// Name of the attribute on the top-level ModuleOp that specifies the IR language name.
 constexpr char LANG_ATTR_NAME[] = "veridise.lang";
+
+/// Name of the attribute on the top-level ModuleOp that specifies the type of the main struct.
+/// This attribute can appear zero or one times on the top-level ModuleOp and is associated with
+/// a `TypeAttr` specifying the `StructType` of the main struct.
+constexpr char MAIN_ATTR_NAME[] = "llzk.main";
 
 } // namespace llzk
--- lib/Util/SymbolHelper.cpp
+++ lib/Util/SymbolHelper.cpp
@@ -305,6 +305,38 @@ FailureOr<SymbolRefAttr> getPathFromTopRoot(FuncDefOp &to, ModuleOp *foundRoot) 
   return RootPathBuilder(RootSelector::FURTHEST, to, foundRoot).getPathFromRootToFunc(to);
 }
 
+FailureOr<StructType> getMainInstanceType(Operation *lookupFrom) {
+  FailureOr<ModuleOp> rootOpt = getRootModule(lookupFrom);
+  if (failed(rootOpt)) {
+    return failure();
+  }
+  ModuleOp root = rootOpt.value();
+  if (Attribute a = root->getAttr(MAIN_ATTR_NAME)) {
+    // If the attribute is present, it must be a TypeAttr of StructType.
+    if (TypeAttr ta = llvm::dyn_cast<TypeAttr>(a)) {
+      return success(llvm::dyn_cast<StructType>(ta.getValue()));
+    }
+    return rootOpt->emitError().append(
+        '"', MAIN_ATTR_NAME, "\" on top-level module must be a '", StructType::name, "' attribute"
+    );
+  }
+  // The attribute is optional so it's okay if not present.
+  return success(nullptr);
+}
+
+FailureOr<SymbolLookupResult<StructDefOp>>
+getMainInstanceDef(SymbolTableCollection &symbolTable, Operation *lookupFrom) {
+  FailureOr<StructType> mainStructTypeOpt = getMainInstanceType(lookupFrom);
+  if (failed(mainStructTypeOpt)) {
+    return failure();
+  }
+  if (StructType st = mainStructTypeOpt.value()) {
+    return st.getDefinition(symbolTable, lookupFrom);
+  } else {
+    return success(nullptr);
+  }
+}
+
 LogicalResult verifyParamOfType(
     SymbolTableCollection &tables, SymbolRefAttr param, Type parameterizedType, Operation *origin
 ) {
